4dfd32ab564b66b384202347d2bd5010b44f9c0e
Yash <yashpc09@gmail.com>
2025-01-02 19:25:57 +0530

    copy pasta

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,16 @@
+use std::collections::HashMap;
+
 pub enum CurrentScreen {
     Main,
     Editing,
     Exiting,
 }
+
 pub enum CurrentlyEditing {
     Key,
     Value,
 }
+
 pub struct App {
     pub key_input: String,              // the currently being edited json key.
     pub value_input: String,            // the currently being edited json value.
@@ -25,6 +29,7 @@ impl App {
             currently_editing: None,
         }
     }
+
     pub fn save_key_value(&mut self) {
         self.pairs
             .insert(self.key_input.clone(), self.value_input.clone());
@@ -33,6 +38,7 @@ impl App {
         self.value_input = String::new();
         self.currently_editing = None;
     }
+
     pub fn toggle_editing(&mut self) {
         if let Some(edit_mode) = &self.currently_editing {
             match edit_mode {
@@ -43,6 +49,7 @@ impl App {
             self.currently_editing = Some(CurrentlyEditing::Key);
         }
     }
+
     pub fn print_json(&self) -> serde_json::Result<()> {
         let output = serde_json::to_string(&self.pairs)?;
         println!("{}", output);
